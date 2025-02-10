bad4d59e5c4c7fdfa2d0780c324e06a395f78323
freetunnel <153220342+freetunnel@users.noreply.github.com>
2025-02-10 20:04:35 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 pkg install nodejs -y
 pkg install ffmpeg -y
 pkg install imagemagick -y
-git clone https://github.com/ilmanhdyt/ShiraoriBOT
+git clone https://github.com/freetunnel/wa
 npm install
 npm update
